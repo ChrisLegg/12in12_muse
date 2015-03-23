bcb78545c5c395f766c0bf5a0abe4a334a81f145
Chris Legg <chris@chrislegg.net>
2015-03-23 15:49:44 +0000

    Convert layour to haml and add base navigation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   acts_as_votable
   belongs_to :user
-  has_attached_file :image, styles: { medium: "700x500#>", thumb: "350x250>" }
+  has_attached_file :image, styles: { medium: "700x500#", thumb: "350x250" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   has_many :comments
 end
